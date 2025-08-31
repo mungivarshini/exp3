029d1c01d440bc33342cd35f2293fec836a692b8
Varshini <mungivarshini1306@gmail.com>
2025-08-31 13:03:44 +0530

    Add basic clientside validation to registration Form

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ document.getElementById("ecetForm").addEventListener("submit", function (e) {
   alert("Form submitted successfully!");
   this.reset(); // Optional: reset the form after submission
 });
+document.getElementById("ecetForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent form from submitting
+
+  const phone = document.getElementById("phone").value;
+
+  if (!/^\d{10}$/.test(phone)) {
+    alert("Phone number must be 10 digits.");
+    return;
+  }
+
+  alert("Form submitted successfully");
+  this.reset(); // Optional: reset the form after submission
+});
